d262aae5b8d3fcebb089f86bbd20ee45263b266a
Egor Fedotov <fedmehol@icloud.com>
2026-08-16 12:02:23 +0300

    fix bug

--- src/main/java/com/efedotov/vaultly/dto/folder/FolderUpdateDto.java
+++ src/main/java/com/efedotov/vaultly/dto/folder/FolderUpdateDto.java
@@ -7,6 +7,5 @@ import lombok.Data;
 public class FolderUpdateDto {
     @NotBlank(message = "Новое имя обязательно")
     private String name;
-
     private String description;
 }
--- src/main/java/com/efedotov/vaultly/model/User.java
+++ src/main/java/com/efedotov/vaultly/model/User.java
@@ -90,7 +90,6 @@ public class User {
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
-    // TOTP
     @Column(name = "totp_secret", length = 64)
     private String totpSecret;
 
