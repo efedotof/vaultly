3382a17069799ee12362a3b1c80e69cf040a81d5
efedotof <fedmehol@icloud.com>
2026-09-23 11:57:58 +0300

    create controllers

---
+++ lib/features/document_viewer/controller/document_viewer_controller.dart
@@ -0,0 +1,6 @@
+
+import 'package:get/get.dart';
+
+class DocumentViewerController extends GetxController {
+  
+}
---
+++ lib/features/notes/controller/note_editor_controller.dart
@@ -0,0 +1,3 @@
+import 'package:get/get.dart';
+
+class NoteEditorController extends GetxController {}
